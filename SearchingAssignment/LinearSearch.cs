8bd817f8f0fe16f60ae372d420a38975b6e35b84
LuisMartinezWD <146053932+LuisMartinezWD@users.noreply.github.com>
2025-12-02 01:21:08 -0600

    Add files via upload

---
+++ SearchingAssignment/LinearSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingAssignment
+{
+    public class LinearSearch
+    {
+        public static int Search(List<int> data, int target)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i] == target)
+                {
+                    return i; 
+                }
+            }
+            return -1; 
+        }
+
+        public static void RunSearch(List<int> data, int target)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            int index = Search(data, target);
+            stopwatch.Stop();
+
+            Console.WriteLine("Linear Search:");
+            Console.WriteLine($"  Result Index: {index}");
+            Console.WriteLine($"  Time: {stopwatch.ElapsedTicks} ticks\n");
+        }
+
+    }
+}
